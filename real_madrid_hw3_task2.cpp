175be5257d42579d3d9dc607e9c3f364402954ef
Tyler <tylersandoval@mail.weber.edu>
2017-09-26 12:54:03 -0600

    Initial commit

---
+++ real_madrid_hw3_task2.cpp
@@ -0,0 +1,17 @@
+// File: real_madrid_hw3_task2
+// Created by Tyler Sandoval on 9/26/2017 for CS1410.
+// Copyright (c) 2017 WSU
+//
+#include <iostream>
+
+using namespace std;
+// Constants, Structs, Classes
+
+// Prototypes
+
+// Main Program Program
+int main(void) {
+    
+    return 0;
+}
+// Function Definitions
