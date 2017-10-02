6cb9f2638b0bfa6c4a7c588485c2dff376e20a8a
sethpalmer94 <sethpalmer@mail.weber.edu>
2017-10-01 18:45:12 -0600

    add get_Date and show_Date

--- real_madrid_hw3_task1.cpp
+++ real_madrid_hw3_task1.cpp
@@ -3,11 +3,25 @@ using namespace std;
 class date
 {
 private:
+	int month, day, year;
+	char tChar
 public:
+	void get_Date()
+	{
+		cout << "enter a date (format mm/dd/yyyy)" << endl;
+		cin >> month >> tchar >> day >> tchar >> year >> endl;
+	}
+	void show_Date()
+	{
+		cout >> month >> "/" >> day >> "/"year << endl;
+	}
+
 };
 int main() 
 {
-    
+	date d1, d2;
+	d1.get_Date();
+	d2.get_Date();
 	
     return 0;
 }
