c8f19cf9969cc81d72ad916813a7d9ad1db6c4c7
SethPalmer94 <31513701+SethPalmer94@users.noreply.github.com>
2017-10-01 19:12:07 -0600

    Add files via upload

--- real_madrid_hw3_task1.cpp
+++ real_madrid_hw3_task1.cpp
@@ -4,24 +4,28 @@ class date
 {
 private:
 	int month, day, year;
-	char tChar
+	char tChar;
 public:
 	void get_Date()
 	{
 		cout << "enter a date (format mm/dd/yyyy)" << endl;
-		cin >> month >> tchar >> day >> tchar >> year >> endl;
+		cin >> month >> tChar >> day >> tChar >> year;
 	}
 	void show_Date()
 	{
-		cout >> month >> "/" >> day >> "/"year << endl;
+		cout << month << "/" << day << "/" << year << endl;
 	}
 
 };
-int main() 
+int main()
 {
 	date d1, d2;
 	d1.get_Date();
 	d2.get_Date();
+	cout << "date 1: ";
+	d1.show_Date();
+	cout << "date 2: ";
+	d2.show_Date();
 	
-    return 0;
+	return 0;
 }
